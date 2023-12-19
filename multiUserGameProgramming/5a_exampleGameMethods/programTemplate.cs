d4aa7a140740878b66e0332775f36ea8e541d4ec
s20118745 <s20118745@students.duvalschools.org>
2023-12-19 13:59:24 -0500

    Update programTemplate.cs

--- multiUserGameProgramming/5a_exampleGameMethods/programTemplate.cs
+++ multiUserGameProgramming/5a_exampleGameMethods/programTemplate.cs
@@ -1,12 +1,80 @@
 // 02_Collections, Alexander Jones, 11-08-2023 v0.0
 using System;
 
-namespace PRogram
+/*
+Your code should include the following items:
+· At least five different variables that are correctly named. [10 Points Each / 50 Total]
+o At least one integer variable.
+o At least one float variable.
+o At least one string variable.
+o At least one Boolean variable.
+o At least one variable must be an array.
+· Use of at least two different assignment operators. [10 Points Each / 20 Total]
+o Assignment Operators: =, +=, -=, *=, /=, and %=
+· At least four different methods. [25 Points Each / 100 Total]
+o Each method should have a comment with a brief description of what the method does, what parameters it requires, and what value it returns.
+o At least one method should have no return value.
+o At least one method should have a return value.
+o At least one method should require output from another method to execute.
+o At least one method must require at least two parameters.
+o At least one method should have a default parameter defined.
+o For each method, the first time you define it, use the pass keyword in the method body before you write any code. MAKE SURE YOUR GITHUB COMMIT SHOWS the pass keyword in your method.
+· Use of at least one if / elif / else block to allow code to make decisions. [25 Points]
+o The logic in your if / elif / else block should be correct.
+· Use at least one while loop. [25 Points]
+· Use at least one for loop to iterate through an array. [25 Points]
+· Frequent use of comments to explain variables and methods. [25 Points]
+
+
+
+flappy bird
+
+float height
+int score
+string player name
+array pipe heights
+pipes have a gap of 60, then 40, 20, 10
+jump += height
+fall -= height
+
+method jump/fall return height
+method randomize pipes height
+method take player name, default "Anonymous"
+method check if height matches pipe
+need variable for next pipe
+
+for loop show nearby pipes
+while loop hold game
+*/
+
+
+
+namespace ExampleGameMethods
 {
-    class Program
-    (
-        static void Main(string[] args)
+    class ExampleGameMethods
+    {
+        static float Move(float height)
+        {
+            //
         }
+
+        static void PlayerName(string playerName = "Anonymous")
+        {
+            //
+        }
+
+        static void RandomNumber(int min, int max)
+        {
+            //
+        } 
+
+        static void CheckPipe()
+        {
+            //
+        }
+
+        static void Main(string[] args)
+        {
 
         }
     }
