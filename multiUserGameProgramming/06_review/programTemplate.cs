1ca8e1215228bf06312de49b8dea1aa5d6e58327
s20118745 <s20118745@students.duvalschools.org>
2024-01-10 14:06:37 -0500

    yeah

---
+++ multiUserGameProgramming/06_review/programTemplate.cs
@@ -0,0 +1,28 @@
+// Alexander Jones, 
+using System;
+
+namespace ReviewProject
+{
+    class ReviewProject
+    {
+        static ReviewProject
+        {
+
+            // Delaring and Assigning Variables
+            // Integer -- -21,984,673,124,234,231
+            // Integer = 4 Bytes
+            Int myInt = 5;
+
+            // Long -- -2,12281638218
+            // Long -8 Bytes
+
+            int myInt = 3.14F;
+            //Float
+            float myFloat =
+            // Boolean
+            bool myBool = true;
+            // string
+            string myString = "Peanut Butter"
+        }
+    }
+}
